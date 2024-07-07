ed50367505e6355cb21371e532963df108facc20
Daevhalla <vitta.smon@gmail.com>
2024-07-07 13:10:23 +0200

    Homepage changes

--- src/data/HomeData.tsx
+++ src/data/HomeData.tsx
@@ -21,6 +21,40 @@ export default function HomeData() {
                 {`It is unknown how misterious it is.`}
             </Link>
         },
+        {
+            title: "What is Turbulenta",
+            text: <>
+                {`Turbulenta is a western marches inspired D&D online campaign where almost anybody can join in.
+                    The game is set on an island of Turbulenta where a new colony from the Iron Rose Kingdom,
+                    or Rosa de Hierro, is established - the town Rosa Riendo being the main settlement.`}<br />
+            </>,
+            link: <Link
+                to="artefacts">
+                {`It is unknown how misterious it is.`}
+            </Link>
+        },
+        {
+            title: "Rules",
+            text: <>
+                {`Beyond the rules of common decency and listening to our DMs and community leaders, here are some useful rules to get you into the game:`}<br />
+            </>,
+            link: <Link
+                to="artefacts">
+                {`Know thy rules`}
+            </Link>
+        },
+        {
+            title: "What is Turbulenta",
+            text: <>
+                {`The D&D game itself is happening on the discord server DigiMC, which is the offical discord server
+                     of Mladi zmaji.`}<br />
+                {`The game is running Amulet D20 which partnered with Mladi zmaji to bring us Turbulenta.`}<br />
+            </>,
+            link: <Link
+                to="artefacts">
+                {`Come and join us.`}
+            </Link>
+        },
     ]
 
     {/*
--- src/types.d.ts
+++ src/types.d.ts
@@ -15,7 +15,7 @@ type RPTags = "sex" | "blood";
 
 type CharNamesS1 = "Remember Me" | "Minamoto" | "Morwyn";
 
-type CharNamesS2 = "Remember Me" | "Minamoto" | "Morwyn";
+type CharNamesS2 = "Remember Me" | "Minamoto" | "Morwyn" | "Ceres" | "Thian" | "Caraxes" | "Jožef" | "Anderson" | "Alvida" | "Bob" | "Parateretes" | "Catalan" | "Janez" | "Davric" | "Rua" | "Coco";
 
 type Seasons = "Season 1" | "Season 2";
 
